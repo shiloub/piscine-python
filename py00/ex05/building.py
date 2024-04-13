13c7ad467e6cb1b93a59409ef3b3ff099e3ac9f2
Achille MONTANTIN <amontant@bess-f2r2s13.clusters.42paris.fr>
2024-04-13 21:27:46 +0200

    ines

--- py00/ex05/building.py
+++ py00/ex05/building.py
@@ -1,35 +1,56 @@
+# import sys
+
+
+# def main():
+#     """program that takes a single string argument and displays the sums
+#      of its upper-case characters, lower-case
+#      characters, punctuation characters, digits and spaces. If
+#      None or nothing is provided, the user is prompted to provide a string."""
+#     if len(sys.argv) == 1:
+#         try:
+#             string = input("What do i have to count ?\n")
+#             string += '\n'
+
+#         except EOFError:
+#                print("i got it")
+#                exit()
+#         #     pass
+#     elif len(sys.argv) == 2:
+#         string = sys.argv[1]
+#     else:
+#         print("AssertionError: more than one argument is provided")
+#         return (0)
+#     print(f"The text contains {len(string)} caracters:")
+#     upper_count = sum(1 for char in string if char.isupper())
+#     lower_count = sum(1 for char in string if char.islower())
+#     digit_count = sum(1 for char in string if char.isdigit())
+#     space_count = sum(1 for char in string if char.isspace())
+#     ponct = ",?;.:/!-_"
+#     ponctiation_count = sum(1 for char in string if char in ponct)
+#     print(upper_count, " upper letters")
+#     print(lower_count, " lower letters")
+#     print(ponctiation_count, " ponctuation marks")
+#     print(space_count, " spaces")
+#     print(digit_count, " digits")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
 import sys
+string = ''
+# while ('\n' not in string):
+#     char = sys.stdin.read(1)
+#     if char == "":
+#         print("oui")
+#         break
+#     string += char
 
+print("oui")
+string = sys.stdin.readline()
+if string == "":
+    print("ehoh")
+print()
 
-def main():
-    """program that takes a single string argument and displays the sums
-     of its upper-case characters, lower-case
-     characters, punctuation characters, digits and spaces. If
-     None or nothing is provided, the user is prompted to provide a string."""
-    if len(sys.argv) == 1:
-        try:
-            str = input("What do i have to count ?\n")
-        except EOFError:
-            pass
-    elif len(sys.argv) == 2:
-        str = sys.argv[1]
-    else:
-        print("AssertionError: more than one argument is provided")
-        return (0)
-    # print(str, " <--")
-    print(f"The text contains {len(str)} caracters:")
-    upper_count = sum(1 for char in str if char.isupper())
-    lower_count = sum(1 for char in str if char.islower())
-    digit_count = sum(1 for char in str if char.isdigit())
-    space_count = sum(1 for char in str if char.isspace())
-    ponct = ",?;.:/!-_"
-    ponctiation_count = sum(1 for char in str if char in ponct)
-    print(upper_count, " upper letters")
-    print(lower_count, " lower letters")
-    print(ponctiation_count, " ponctuation marks")
-    print(space_count, " spaces")
-    print(digit_count, " digits")
-
-
-if __name__ == "__main__":
-    main()
+print(string)
