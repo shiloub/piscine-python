5bcbb2cdd718dfa8d427625c1a29b7d541a1094d
Achille MONTANTIN <amontant@bess-f2r1s4.clusters.42paris.fr>
2024-04-15 19:11:40 +0200

    message

--- py01/ex02/load_image.py
+++ py01/ex02/load_image.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def ft_load(path: str):
     try:
+        assert path.lower().endswith((".jpg", ".jpeg")), "Wrong file extention"
         img = Image.open(path)
-    except (FileNotFoundError, PermissionError, UnidentifiedImageError) as error:
+    except (FileNotFoundError, PermissionError, UnidentifiedImageError, AssertionError) as error:
         print("Error: ", error)
         return (None)
     array = np.array(img)
     print("The shape of image is:", array.shape)
     return array
-#your code 
--- py01/ex02/tester.py
+++ py01/ex02/tester.py
@@ -1,4 +1,5 @@
 from load_image import ft_load
 
 print(ft_load("../images/landscape.jpg"))
+print(ft_load("../images/corrupted.jpg"))
 print(ft_load("../images/nyancat.gif"))
